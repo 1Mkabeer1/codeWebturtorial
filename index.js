839f8db347ff1a3ac7428fe00cd091831f2080a2
1Mkabeer1 <mkabeeru9@gmail.com>
2023-02-27 23:07:30 +0100

    ejs-practice

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 const express = require('express');
-// const { path } = require('express/lib/application');
 const { param } = require('express/lib/request');
-// const paths = require(path);
+const path = require('path');
 const app = express()
 const port = 4000;
+
 app.set("view engine", "ejs");
-// app.set("views", paths.join("__dirname", "/views"));
-
-/*
- #first expirament under expressjs turtorial 
-  discuss about routing, parameter and query string.
-
-app.get('/',(req, res) => {
-    res.send('<h1>Hello, My first server</h1>')
-    console.log('Hello, This is my first server');
-});
-
-app.get("/:parameter", (req, res) => {
-    const {parameter} = req.params;
-    const {q} = req.query;
-    console.log(req.params);
-    console.log(req.query);
-    res.send(`<h1>Your Browsing under ${parameter} parameter searching for ${q} Query.</h1>`);
-});
-
-app.post("/", (req,res) => {
-    res.send("Posted successful!")
-});
-*/
+app.set('views', path.join(__dirname, 'views'))
 
 app.get("/", (req,res) => {
-    res.render("home")
+    res.render(`home`)
 })
 app.get("*",(req,res) => {
     res.send("Path not available")
